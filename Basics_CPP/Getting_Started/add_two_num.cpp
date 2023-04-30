3d1e6766d5225ffeaea0990a2bb8216d114afb17
Aman Mittal <amanmittalma@gmail.com>
2023-04-30 16:40:08 +0530

    Variables

---
+++ Basics_CPP/Getting_Started/add_two_num.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /* Variable and Data Types 1. Integer*/
+    int a = 10;
+    int b = 15;
+    int c = a + b;
+
+    cout << c << endl;
+     /* Chracter*/
+    char d = 'x';
+    cout << d << endl;
+
+    float f = 1.23;
+    cout << f << endl;
+
+    bool bl = true;
+    cout << bl << endl;
+
+    cout << sizeof(a) << endl;
+}
