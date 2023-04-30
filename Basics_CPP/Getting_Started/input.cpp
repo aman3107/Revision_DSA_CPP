054fb47492a8cdfd376f33dc5fcf30c3989bb5bc
Aman Mittal <amanmittalma@gmail.com>
2023-04-30 18:17:28 +0530

    input

---
+++ Basics_CPP/Getting_Started/input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    cout << "Enter values of a  and b :" << endl;
+    cin >> a >> b;
+    int c = a + b;
+    cout <<"Value of c : "<< c << endl;
+    int d;
+    cout << "Value of d : " << d << endl;
+}
