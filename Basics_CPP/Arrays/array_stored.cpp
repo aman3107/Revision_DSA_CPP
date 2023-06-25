8a0eb47eddfde1120560cb78777dd105278573f7
Aman Mittal <amanmittalma@gmail.com>
2023-06-25 18:36:06 +0530

    Arrays Stored

---
+++ Basics_CPP/Arrays/array_stored.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[],int n){
+    cout << "Function :" << sizeof(arr) << endl;
+    // for(int i = 0; i < n; i++){
+    //     cout << arr[i];
+    // }
+}
+
+int main(){
+    int n;
+    cout << "Enter value of n : " << endl;
+    cin >> n;
+    int arr[100];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cout << "Main :" << sizeof(arr) << endl;
+    printArray(arr,n);
+
+}
