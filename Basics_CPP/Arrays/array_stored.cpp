bd719eeab7f6891703bc3075a4b540b59c17c25b
Aman Mittal <amanmittalma@gmail.com>
2023-06-26 11:31:49 +0530

    Linear Search

--- Basics_CPP/Arrays/array_stored.cpp
+++ Basics_CPP/Arrays/array_stored.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void printArray(int arr[],int n){
     cout << "Function :" << sizeof(arr) << endl;
-    // for(int i = 0; i < n; i++){
-    //     cout << arr[i];
-    // }
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
 }
 
 int main(){
@@ -13,6 +13,7 @@ int main(){
     cout << "Enter value of n : " << endl;
     cin >> n;
     int arr[100];
+    cout << "Enter elements of array :" << endl;
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
