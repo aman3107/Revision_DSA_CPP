54afb215b9c46e0abd04ab5ca3e9bc042a20a96d
Aman Mittal <amittal@mirrahealthcare.com>
2024-01-15 17:00:33 +0530

    Practice

---
+++ Basics_CPP/Test1/Practice.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  /* Pyramid Number Pattern */
+  // int n;
+  // cout << "Enter value of n :" << endl;
+  // cin >> n;
+  // int i = 1;
+  // while (i <= n)
+  // {
+  //   int spaces = 1;
+  //   while (spaces <= n - i)
+  //   {
+  //     cout << " ";
+  //     spaces++;
+  //   }
+  //   int j = i;
+  //   while (j > 0)
+  //   {
+  //     cout << j;
+  //     j--;
+  //   }
+  //   j = 2;
+  //   while (j <= i)
+  //   {
+  //     cout << j;
+  //     j++;
+  //   }
+  //   cout << endl;
+  //   i++;
+  // }
+
+  /* Number Star Pattern */
+  // int n;
+  // cout << "Enter value of n :" << endl;
+  // cin >> n;
+
+  // int i = 1;
+  // while (i <= n)
+  // {
+  //   int j = 1;
+  //   while (j <= (n - i + 1))
+  //   {
+  //     cout << j;
+  //     j++;
+  //   }
+  //   int star = 2 * (i - 1);
+  //   j = 1;
+  //   while (j <= star)
+  //   {
+  //     cout << '*';
+  //     j++;
+  //   }
+  //   j = 1;
+  //   while (j <= (n - i + 1))
+  //   {
+  //     cout << j;
+  //     j++;
+  //   }
+  //   cout << endl;
+  //   i++;
+  // }
+
+  /* Second Largest */
+  // int n;
+  // cout << "Enter number of elements : " << endl;
+  // cin >> n;
+  // int first_lar = INT_MIN;
+  // int second_lar = INT_MIN;
+  // int num;
+  // for (int i = 1; i <= n; i++)
+  // {
+  //   cin >> num;
+  //   if (num > first_lar)
+  //   {
+  //     second_lar = first_lar;
+  //     first_lar = num;
+  //   }
+  //   else if (num > second_lar && num != first_lar)
+  //   {
+  //     second_lar = num;
+  //   }
+  // }
+  // cout << second_lar;
+}
