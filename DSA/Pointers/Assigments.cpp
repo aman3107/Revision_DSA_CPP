832ee5040479cc6659fb7e294a90ccd8378e07ef
Aman Mittal <amanmittalma@gmail.com>
2024-02-13 23:41:47 +0530

    dymanic allocation practice

--- DSA/Pointers/Assigments.cpp
+++ DSA/Pointers/Assigments.cpp
@@ -26,20 +26,48 @@ void moveZerosToEnd(int *arr, int n)
   }
 }
 
+void transposeMatrix(int **arr, int m, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      cout << arr[j][i] << " ";
+    }
+    cout << endl;
+  }
+}
+
 int main()
 {
-  int n;
-  cin >> n;
-  int *arr = new int[n];
-  for (int i = 0; i < n; i++)
+  int m, n;
+  cin >> m >> n;
+  int **arr = new int *[m];
+  for (int i = 0; i < m; i++)
   {
-    cin >> arr[i];
+    arr[i] = new int[n];
+    for (int j = 0; j < n; j++)
+    {
+      cin >> arr[i][j];
+    }
   }
-
-  moveZerosToEnd(arr, n);
-  for (int i = 0; i < n; i++)
+  transposeMatrix(arr, m, n);
+  for (int i = 0; i < m; i++)
   {
-    cout << arr[i] << " ";
+    delete[] arr[i];
   }
   delete[] arr;
+  // int n;
+  // cin >> n;
+  // int *arr = new int[n];
+  // for (int i = 0; i < n; i++)
+  // {
+  //   cin >> arr[i];
+  // }
+  // moveZerosToEnd(arr, n);
+  // for (int i = 0; i < n; i++)
+  // {
+  //   cout << arr[i] << " ";
+  // }
+  // delete[] arr;
 }
