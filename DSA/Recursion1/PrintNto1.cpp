5477402b5026738f5939e696376a0685e23fe4cc
Aman Mittal <amanmittalma@gmail.com>
2024-02-29 07:14:37 +0530

    Recusrion Start

---
+++ DSA/Recursion1/PrintNto1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void printNto1(int n)
+{
+  if (n < 1)
+  {
+    return;
+  }
+  cout << n << endl;
+  return printNto1(n - 1);
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  printNto1(n);
+}
