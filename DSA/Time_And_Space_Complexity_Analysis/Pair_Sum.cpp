212abaf4356ffba05372e0b91ba6812e0dee8a3e
Aman Mittal <amanmittalma@gmail.com>
2024-03-22 18:35:41 +0530

    Pair Sum

---
+++ DSA/Time_And_Space_Complexity_Analysis/Pair_Sum.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int pairSum3(int *arr, int n, int x)
+{
+  // Write your code here
+  sort(arr, arr + n);
+  int count = 0;
+  int i = 0;
+  int j = n - 1;
+  while (i < j)
+  {
+    if (arr[i] + arr[j] == x)
+    {
+      count++;
+      int k = i + 1;
+      while (k <= j - 1 && arr[i] == arr[k])
+      {
+        if (arr[i] + arr[k] == x)
+        {
+          count++;
+          k++;
+        }
+        else
+        {
+          break;
+        }
+      }
+      if (arr[i] == arr[i + 1])
+      {
+        i++;
+      }
+      else
+      {
+        i++;
+        j--;
+      }
+    }
+    else if (arr[i] + arr[j] < x)
+    {
+      i++;
+    }
+    else
+    {
+      j--;
+    }
+  }
+  return count;
+}
+
+int pairSum2(int arr[], int n, int x)
+{
+  sort(arr, arr + n);
+  int count = 0;
+  int i = 0;
+  int j = n - 1;
+  while (i < j)
+  {
+    if (arr[i] + arr[j] == x)
+    {
+      if (arr[i] == arr[j])
+      {
+        int countPairs = (j - i + 1);
+        count += (countPairs * (countPairs - 1) / 2);
+        return count;
+      }
+      else
+      {
+        count++;
+        if (arr[i] == arr[i + 1])
+        {
+          i++;
+        }
+        else
+        {
+          i++;
+          j--;
+        }
+      }
+    }
+    else if (arr[i] + arr[j] < x)
+    {
+      i++;
+    }
+    else
+    {
+      j--;
+    }
+  }
+  return count;
+}
+
+int pairSum(int arr[], int n, int x)
+{
+  sort(arr, arr + n);
+  int count = 0;
+  int i = 0;
+  int j = n - 1;
+  while (i < j)
+  {
+    if (arr[i] + arr[j] == x)
+    {
+      count++;
+      i++;
+      j--;
+    }
+    else if (arr[i] + arr[j] < x)
+    {
+      i++;
+    }
+    else
+    {
+      j--;
+    }
+  }
+  return count;
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  int *arr = new int[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  int x;
+  cin >> x;
+  cout << pairSum2(arr, n, x) << endl;
+  delete[] arr;
+}
