f132099adc7838cef288a9191ae840868ca8a97c
Aman Mittal <amanmittalma@gmail.com>
2024-08-11 16:47:57 +0530

    Square, Num Pattern

---
+++ Basics_CPP/Revision/NumberPatterns.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int n;
+  cout << "Enter number of rows and columns" << endl;
+  cin >> n;
+  int i = 0;
+  while (i < n)
+  {
+    int j = 0;
+    while (j < n)
+    {
+      cout << n - j << " ";
+      j++;
+    }
+    cout << endl;
+    i++;
+  }
+}
