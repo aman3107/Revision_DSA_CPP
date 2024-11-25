95ee50800b9b4c5748833c628aa20b2a704d7db4
Aman Mittal <amanmittalma@gmail.com>
2024-11-25 23:49:42 +0530

    Getters and Setters

--- DSA/OOPS-1/Student.cpp
+++ DSA/OOPS-1/Student.cpp
@@ -6,4 +6,29 @@ class Student
 public:
   int rollNo;
   int age;
+
+private:
+  char name[100];
+  int phoneNumber;
+
+public:
+  void display()
+  {
+    cout << age << " " << rollNo << endl;
+  }
+
+  int getAge()
+  {
+    return age;
+  }
+
+  int getPhoneNumber()
+  {
+    return phoneNumber;
+  }
+
+  void setPhoneNumber(int number)
+  {
+    phoneNumber = number;
+  }
 };
--- DSA/OOPS-1/StudentUse.cpp
+++ DSA/OOPS-1/StudentUse.cpp
@@ -11,7 +11,15 @@ int main()
   // Create Objects Dynamically
   Student *s2 = new Student;
   s1.age = 10;
-  cout << s1.age;
+  cout << s1.age << endl;
+  s1.rollNo = 101;
   (*s2).age = 24;
   s2->rollNo = 102;
+  // s1.name = 'Aman' // It will gives us te error
+  s1.display();
+  s2->display();
+  cout << s1.getAge() << endl;
+  cout << s1.getPhoneNumber() << endl;
+  s1.setPhoneNumber(12345678);
+  cout << s1.getPhoneNumber() << endl;
 }
