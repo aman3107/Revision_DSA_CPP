e0f294e6e05a522886fcc67addb688ac4645d66e
Aman Mittal <amanmittalma@gmail.com>
2024-11-26 23:42:47 +0530

    Costructor Destructor

---
+++ DSA/OOPS-1/Constructor.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "Student.cpp"
+using namespace std;
+
+int main()
+{
+  // Constructor
+  // Student s1;
+  // Student *s2 = new Student;
+  // s1.display();
+  // s2->display();
+  // Student s3(20);
+  // s3.display();
+
+  // Student *s4 = new Student(22);
+  // cout << "Student s4 address :" << s4 << endl;
+  // s4->display();
+
+  // Student s5(20, 102);
+  // s5.display();
+
+  // Copy Constructor
+
+  // Student s1(10, 20);
+  // Student s2(s1);
+  // s1.display();
+  // s2.display();
+
+  // Student *s3 = new Student(10, 200);
+  // Student s4(*s3);
+  // Student *s5 = new Student(*s3);
+  // s3->display();
+  // s4.display();
+  // s5->display();
+
+  // Copy Assignment Constructor
+
+  Student s1(10, 200);
+  Student s2(20, 100);
+  Student *s3 = new Student(30, 300);
+  s2 = s1;
+  *s3 = s1;
+  s1.display();
+  s3->display();
+  s2.display();
+  delete s3;
+  Student s4 = s1; // Student s4(s1); Cop Constructor Called
+}
--- DSA/OOPS-1/Student.cpp
+++ DSA/OOPS-1/Student.cpp
@@ -12,6 +12,31 @@ private:
   int phoneNumber;
 
 public:
+  // Desturctor
+  ~Student()
+  {
+    cout << "Destructor is called :" << endl;
+  }
+  // Default Constructor
+  Student()
+  {
+    cout << "Constructor Called :" << endl;
+  }
+
+  // Parameterized Constructor
+  Student(int rollNo)
+  {
+    cout << "this : " << this << endl;
+    cout << "Constructor 2 called: " << endl;
+    this->rollNo = rollNo;
+  }
+
+  Student(int a, int r)
+  {
+    cout << "Constructor 3 called: " << endl;
+    age = a;
+    rollNo = r;
+  }
   void display()
   {
     cout << age << " " << rollNo << endl;
