da70c24433e390c075ba249e031518acbe14633a
Aman Mittal <amanmittalma@gmail.com>
2024-12-03 23:26:07 +0530

    OOPS-2

---
+++ DSA/OOPS-2/Student.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+  int age;
+  char *name;
+
+public:
+  Student(int age, char *name)
+  {
+    this->age = age;
+    this->name = name;
+  }
+
+  void display()
+  {
+    cout << name << " " << age << endl;
+  }
+};
