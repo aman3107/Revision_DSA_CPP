12e8b4b89ce27d5672829a389186c63e678fde18
Aman Mittal <amanmittalma@gmail.com>
2024-12-05 23:00:13 +0530

    Copy Construtor Deep Copy

--- DSA/OOPS-2/Student.cpp
+++ DSA/OOPS-2/Student.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class Student
 {
   int age;
+
+public:
   char *name;
 
 public:
   Student(int age, char *name)
   {
     this->age = age;
-    this->name = name;
+    // Shallow Copy
+    // this->name = name;
+
+    // Deep Copy
+    this->name = new char[strlen(name) + 1];
+    strcpy(this->name, name);
+  }
+
+  Student(Student const &s)
+  {
+    this->age = s.age;
+    this->name = new char[strlen(s.name) + 1];
+    strcpy(this->name, s.name);
   }
 
   void display()
--- DSA/OOPS-2/StudentUse.cpp
+++ DSA/OOPS-2/StudentUse.cpp
@@ -8,9 +8,13 @@ int main()
   Student s1(20, name);
   s1.display();
 
-  name[3] = 'e';
-  Student s2(24, name);
+  Student s2(s1);
+  s2.name[0] = 'x';
   s2.display();
-
   s1.display();
+  // name[3] = 'e';
+  // Student s2(24, name);
+  // s2.display();
+
+  // s1.display();
 }
