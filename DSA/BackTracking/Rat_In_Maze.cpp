558e93518432e500e6fa5de3c7ee2bd57eca0c44
Aman Mittal <amanmittalma@gmail.com>
2025-01-30 21:34:29 +0530

    Rat in a maze new solution

--- DSA/BackTracking/Rat_In_Maze.cpp
+++ DSA/BackTracking/Rat_In_Maze.cpp
@@ -2,6 +2,31 @@
 #include <vector>
 using namespace std;
 
+void searchMazeHelper2(vector<vector<int>> &arr, int n, int r, int c, vector<string> &ans, string visited)
+{
+  if (r == n - 1 && c == n - 1 && arr[r][c] == 1)
+  {
+    ans.push_back(visited);
+    return;
+  }
+  if (r < 0 || c < 0 || r >= n || c >= n || arr[r][c] == 0)
+    return;
+  arr[r][c] = 0;
+  searchMazeHelper2(arr, n, r + 1, c, ans, visited + "D");
+  searchMazeHelper2(arr, n, r, c - 1, ans, visited + "L");
+  searchMazeHelper2(arr, n, r, c + 1, ans, visited + "R");
+  searchMazeHelper2(arr, n, r - 1, c, ans, visited + "U");
+  arr[r][c] = 1;
+}
+
+vector<string> searchMaze2(vector<vector<int>> &arr, int n)
+{
+  // Write your code here.
+  vector<string> ans;
+  searchMazeHelper2(arr, n, 0, 0, ans, "");
+  return ans;
+}
+
 bool isValidMove(int y, int x, vector<vector<int>> &arr, vector<vector<bool>> &visited)
 {
   if (x == -1 || x == arr.size() || y == -1 || y == arr.size() || visited[y][x] || arr[y][x] == 0)
